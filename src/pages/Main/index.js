67de174405cfd4d9081ae0a816d432cd8522d056
Victor Fontenele <victorvitoria11@hotmail.com>
2020-03-11 00:10:51 -0300

    main folder - styled components

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,25 +1,12 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#333',
-  },
-  welcome: {
-    color: '#fff',
-    fontSize: 20,
-    textAlign: 'center'
-  }
-});
+import { Container, Text } from './styles';
 
 export default function Main() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Main</Text>
-      </View>
+      <Container>
+          <Text>Styled Component</Text>
+      </Container>
     );
 };
 
---
+++ src/pages/Main/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+    padding: 30px;
+`;
+
+export const Text = styled.Text`
+    color: #7159c1;
+`;
