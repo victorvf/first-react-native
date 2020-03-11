9c6cffceda75ec264ed799b46b8984b7ce275936
Victor Fontenele <victorvitoria11@hotmail.com>
2020-03-11 11:02:51 -0300

    form styles

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,12 +1,32 @@
 import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled.View`
     flex: 1;
-    align-items: center;
-    justify-content: center;
     padding: 30px;
 `;
 
-export const Text = styled.Text`
-    color: #7159c1;
+export const Form = styled.View`
+    flex-direction: row;
+    padding-bottom: 20px;
+    border-bottom-width: 1px;
+    border-color: #BDBDBD;
+`;
+
+export const Input = styled.TextInput`
+    flex: 1;
+    height: 40px;
+    background: #eee;
+    border-radius: 4px;
+    padding: 0 15px;
+    border: 1px solid #BDBDBD;
+`;
+
+export const SubmitButton = styled(RectButton)`
+    align-items: center;
+    justify-content: center;
+    background: #7159c1;
+    border-radius: 4px;
+    margin-left: 10px;
+    padding: 0 12px;
 `;
